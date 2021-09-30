acc273aef1f6dfeb3e6891bd4f49ec4f6f1f8ada
Aleksey Kobel <aleksey.kobel@student.tuke.sk>
2021-09-30 09:48:00 +0200

    reactor updates

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -7,13 +7,16 @@ public class Reactor extends AbstractActor {
     private int temperature;
     private int damage;
     private Animation normalAnimation;
+    private Animation hotAnimation;
+    private Animation brokenAnimation;
 
-    public Reactor(Animation normalAnimation){
+    public Reactor(){
         this.temperature = 0;
         this.damage = 0;
-        normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        this.normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        this.hotAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.05f, Animation.PlayMode.LOOP_PINGPONG);
+        this.brokenAnimation = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
         setAnimation(normalAnimation);
-        this.normalAnimation = normalAnimation;
     }
 
     public int getTemperature(){
@@ -25,30 +28,42 @@ public class Reactor extends AbstractActor {
     }
 
     public void updateAnimation(){
-        if(this.temperature == 6000){
-            this.normalAnimation = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-            setAnimation(this.normalAnimation);
-        }
-        else if(this.temperature >= 4000 && this.temperature < 6000){
-            this.normalAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-            setAnimation(this.normalAnimation);
-        }
-        else if(this.temperature < 4000){
-            this.normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        if(this.temperature < 4000){
             setAnimation(normalAnimation);
         }
+        else if(this.temperature < 6000){
+            setAnimation(hotAnimation);
+        }
+        else{
+            setAnimation(brokenAnimation);
+        }
+
+
     }
 
     public void increaseTemperature(int increment){
+        if(increment < 0) return;
+        if(this.damage > 100) return;
+        double newIncrement;
+        if(this.damage >= 33 && this.damage <= 66){
+            newIncrement = (double)increment * 1.5;
+            newIncrement = Math.round(newIncrement);
+            increment = (int)newIncrement;
+        }
+        if(this.damage > 66){
+            increment *= 2;
+        }
         this.temperature = this.temperature + increment;
         if(this.temperature > 2000){
             float newDamage = (float)(this.temperature - 2000)/40;
             this.damage = (int)Math.floor(newDamage);
+            if(this.damage > 100) this.damage = 100;
             updateAnimation();
         }
     }
 
     public void decreaseTemperature(int decrement){
+        if(decrement < 0) return;
         if(this.damage == 100){
             return;
         }
