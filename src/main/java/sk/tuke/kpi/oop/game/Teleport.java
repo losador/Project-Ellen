b5e489c98cbdb73b1967221a107c1e26d89a0455
Aleksey Kobel <aleksey.kobel@student.tuke.sk>
2021-10-30 21:07:47 +0200

    fixes

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -1,6 +1,5 @@
 package sk.tuke.kpi.oop.game;
 
-import sk.tuke.kpi.gamelib.Disposable;
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.actions.When;
@@ -13,9 +12,7 @@ import java.util.Objects;
 
 public class Teleport extends AbstractActor {
     private Teleport destTeleport;
-    private int checkCounter = 0;
     private boolean isActive;
-    private Disposable disposeTp;
 
     public Teleport(Teleport teleport){
         this.destTeleport = teleport;
@@ -31,11 +28,6 @@ public class Teleport extends AbstractActor {
     public Teleport getDestination(){
         if(this.destTeleport == null) return null;
         return this.destTeleport;
-    }
-
-    private boolean isInMiddle(Player player){
-        if(player.getPosX() == this.getPosX() + (this.getWidth()/6) && player.getPosY() == this.getPosY() + (this.getHeight()/6)) return true;
-        return false;
     }
 
     private boolean check(Player player){
@@ -85,6 +77,6 @@ public class Teleport extends AbstractActor {
     public void addedToScene(Scene scene){
         super.addedToScene(scene);
         Player player = (Player) Objects.requireNonNull(getScene()).getFirstActorByName("Player");
-        this.disposeTp = new Loop<>(new Invoke<>(this::teleportOn)).scheduleFor(player);
+        new Loop<>(new Invoke<>(this::teleportOn)).scheduleFor(player);
     }
 }
